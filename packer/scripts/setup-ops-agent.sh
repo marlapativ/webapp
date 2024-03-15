3cad21fa0fee59d59dc0f1cb4d524be7f9fbfff1
Marlapati Venkata Naga Sai Teja[marlapativ] <marlapati.v@northeastern.edu>
2024-03-15 12:39:17 -0400

    Setup Ops Agent

--- packer/scripts/setup-ops-agent.sh
+++ packer/scripts/setup-ops-agent.sh
@@ -4,11 +4,11 @@
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
+# Stop the Ops Agent
+sudo systemctl stop google-cloud-ops-agent
+
 # Printing the status of the Ops Agent
 sudo systemctl status google-cloud-ops-agent"*"
 
 # Copy the config file
 cp -f /tmp/setup/config.yaml /etc/google-cloud-ops-agent/config.yaml
-
-# Restart the Ops Agent
-sudo systemctl restart google-cloud-ops-agent
