b00f29c8e1a195f567370425c0de5696e24462f0
Marlapati Venkata Naga Sai Teja[marlapativ] <marlapati.v@northeastern.edu>
2024-03-19 21:35:18 -0400

    Assignment 6 changes

--- packer/scripts/setup-ops-agent.sh
+++ packer/scripts/setup-ops-agent.sh
@@ -9,9 +9,3 @@ sudo systemctl stop google-cloud-ops-agent
 
 # Copy the config file
 cp -f /tmp/setup/config.yaml /etc/google-cloud-ops-agent/config.yaml
-
-# Restart the Ops Agent
-sudo systemctl restart google-cloud-ops-agent
-
-# Printing the status of the Ops Agent
-sudo systemctl status google-cloud-ops-agent
